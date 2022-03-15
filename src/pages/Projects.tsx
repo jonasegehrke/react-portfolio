68c656cd0203dcfc6e57e3757a7228629b67fe8f
jonasegehrke <jonasegehrke@gmail.com>
2022-03-15 15:09:10 +0100

    changed text in projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -12,8 +12,8 @@ export default function Projects() {
                 <NewPageHeading heading="Projects" />
                 <p>A few projects</p>
                 <div className="projects-container">
-                    <Card title={"Node Notes"} text={"SSR (Server Side Rendered) web app with express. Collection of notes from elective on my education."} href={'https://github.com/jonasegehrke/Node_Mandatory_1'} icon={<FaNodeJs />} />
-                    <Card title={"Portfolio"} text={"This is the web app you are currently on. It is one of my first large projects using react."} href={'https://github.com/jonasegehrke/react-portfolio'} icon={<BsFileEarmarkPerson />} />
+                    <Card title={"Node Notes"} text={"Collection of notes from elective on my education. SSR (Server Side Rendered) web app with express."} href={'https://github.com/jonasegehrke/Node_Mandatory_1'} icon={<FaNodeJs />} />
+                    <Card title={"Portfolio"} text={"This is the web app you are currently on. It is one of my first large projects using react.js & TypeScript."} href={'https://github.com/jonasegehrke/react-portfolio'} icon={<BsFileEarmarkPerson />} />
                     <Card title={"Discord Bot"} text={"discord.js bot fetching stats from game 'Warzone'. Using a backend for live-stat change announcements."} href={'https://github.com/jonasegehrke/WarzoneStatsBot'} icon={<FaDiscord />} />
                     <Card title={"Cinema Administration"} text={"Web app created with Java Spring & static frontend. Full CRUD for movie-screenings, ticket sales & kiosk sales."} href={'https://github.com/jonasegehrke/KinoXP'} icon={<MdLocalMovies />} />
                 </div>
